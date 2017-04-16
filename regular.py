28c46463c7527f944cff89ab2f7147fcb36167b2
WyatPku <1600012704@pku.edu.cn>
2017-04-16 22:18:31 +0800

    fix naocan bug

--- regular.py
+++ regular.py
@@ -6,6 +6,13 @@ try:
 	
 	filter=r"^(?![^A-Z]*$|[^a-z]*$|[^0-9]*$|\w*$)((..)*.)$"
 	#匹配包含大写字母、小写字母、数字和其它字符的长度为奇数的一行
+	output.write("case 1:\n")
+	for it in re.finditer(filter, src, re.M):
+		output.write(it.group()+'\n')
+
+	filter=r"^([^\\]*\\([0abtvfe\\]|r\\n)[^\\]*)*$"
+	#匹配带转义字符的表达式，要求符合windows的换行（\r\n）
+	output.write("case 2:\n")
 	for it in re.finditer(filter, src, re.M):
 		output.write(it.group()+'\n')
 	
